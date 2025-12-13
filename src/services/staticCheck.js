eb84f1d090af5e2a36f34baf286afd0cfedb8ac1
maddy-aleti <minimalistnietzsche@gmail.com>
2025-12-13 16:08:02 +0530

    modified staticCheck.js

--- src/services/staticCheck.js
+++ src/services/staticCheck.js
@@ -163,8 +163,4 @@ export const runStaticCheck = (repoPath) => {
     Object.values(metrics.layerCounts).filter(v => v > 0).length >= 2;
 
   return metrics;
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 13a94a6e413bd59e20c1eec07b1be97b95f259c9
